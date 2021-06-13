df43d1b8a1168b8a4fdd61e8f795e0fc633ab61e
Nicander Mohrmann <s1103064@student.windesheim.nl>
2021-06-13 17:10:01 +0200

    Working base

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -8,14 +8,37 @@ public class BallMovement : MonoBehaviour
     [SerializeField] private float m_speed;
     private Rigidbody2D m_rigidbody2D;
     private Vector2 m_direction = new Vector2(.5f, -1);
+    private float m_hMin;
+    private float m_hMax;
+    private float m_vMin;
+    private float m_vMax;
 
     private void Awake()
     {
         m_rigidbody2D = GetComponent<Rigidbody2D>();
+        Vector2 screenSizeHalved = GameController.ScreenSizeHalved();
+        m_hMin = -screenSizeHalved.x;
+        m_hMax = screenSizeHalved.x;
+        m_vMin = -screenSizeHalved.y;
+        m_vMax = screenSizeHalved.y;
     }
 
     private void FixedUpdate()
     {
+        if (transform.position.y < m_vMin)
+        {
+            Debug.Log("Lost");
+            return;
+        }
+        if (transform.position.x < m_hMin || transform.position.x > m_hMax)
+        {
+            m_direction.x *= -1;
+        }
+
+        if (transform.position.y > m_vMax)
+        {
+            m_direction.y *= -1;
+        }
         m_controller.Move(m_direction * m_speed);
     }
 
@@ -26,11 +49,7 @@ public class BallMovement : MonoBehaviour
             float closestPoint = other.ClosestPoint(transform.position).x;
             float pos = other.transform.position.x;
             float hitPos = closestPoint - pos;
-            Debug.Log(closestPoint);
-            Debug.Log(pos);
-            Debug.Log(hitPos);
             m_direction.y *= -1;
-            float bounce = m_direction.x + hitPos;
             m_direction.x = hitPos;
         }
     }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public static GameController Singleton;
+
+    [SerializeField] private Camera m_mainCamera = null;
     private void Awake()
     {
-
+        InitializeSingleton();
     }
 
+    private void InitializeSingleton()
+    {
+        if (Singleton != null && Singleton != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Singleton = this;
+        }
+    }
 
+    public static Vector2 ScreenSizeHalved()
+    {
+        float halfHeight = Singleton.m_mainCamera.orthographicSize;
+        float halfWidth = Singleton.m_mainCamera.aspect * halfHeight;
+        return new Vector2(halfWidth, halfHeight);
+    }
 }
